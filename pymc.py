ed1d405c2eff91ff27ca2a994f33c70b80b2fa09
schubisu <robin.schubert@ghi-muenster.de>
2017-06-15 10:51:33 +0200

    fist round of bugfixes

--- pymc.py
+++ pymc.py
@@ -57,47 +57,47 @@ class PyMC():
         return 0
 
     def read_pymc(self):
-        (status, uid) = self.authenticate(settings.PYMC_BLOCK)
+        (status, uid) = self.authenticate(PYMC_BLOCK)
         if status == self.MIFAREReader.MI_OK:
-            pymcdata = self.read_block(settings.PYMC_BLOCK)
+            pymcdata = self.read_block(PYMC_BLOCK)
             GPIO.cleanup()
             if not pymcdata == 1:
-                self.playlist_number = pymcdata[settings.PLAYLIST_NUMBER]
-                self.track_number = pymcdata[settings.TRACK_NUMBER]
-                self.playlist_repeat = pymcdata[settings.PlAYLIST_REPEAT]
-                self.playlist_shuffle = pymcdata[settings.PLAYLIST_SHUFFLE]
+                self.playlist_number = pymcdata[PLAYLIST_NUMBER]
+                self.track_number = pymcdata[TRACK_NUMBER]
+                self.playlist_repeat = pymcdata[PlAYLIST_REPEAT]
+                self.playlist_shuffle = pymcdata[PLAYLIST_SHUFFLE]
 
     def write_pymc(self):
-        (status, uid) = self.authenticate(settings.PYMC_BLOCK)
+        (status, uid) = self.authenticate(PYMC_BLOCK)
         if status == self.MIFAREReader.MI_OK:
             pymcdata = [0] * 16
-            pymcdata[settings.PLAYLIST_NUMBER] = self.playlist_number
-            pymcdata[settings.TRACK_NUMBER] = self.track_number
-            pymcdata[settings.PlAYLIST_REPEAT] = self.playlist_repeat
-            pymcdata[settings.PLAYLIST_SHUFFLE] = self.playlist_shuffle
-            self.write_block(settings.PYMC_BLOCK, pymcdata)
+            pymcdata[PLAYLIST_NUMBER] = self.playlist_number
+            pymcdata[TRACK_NUMBER] = self.track_number
+            pymcdata[PlAYLIST_REPEAT] = self.playlist_repeat
+            pymcdata[PLAYLIST_SHUFFLE] = self.playlist_shuffle
+            self.write_block(PYMC_BLOCK, pymcdata)
             GPIO.cleanup()
 
     def connect_mpd(self):
-        self.mpd.connect(settings.MPD_IP, settings.MPD_PORT)
+        self.mpd.connect(MPD_IP, MPD_PORT)
 
     def play_next(self):
         self.connect_mpd()
         self.mpd.next()
-        status, uid = self.authenticate(settings.PYMC_BLOCK)
+        status, uid = self.authenticate(PYMC_BLOCK)
         if str(uid) == self.uid:
             self.mpd_to_card()
 
     def play_previous(self):
         self.connect_mpd()
         self.mpd.previous()
-        status, uid = self.authenticate(settings.PYMC_BLOCK)
+        status, uid = self.authenticate(PYMC_BLOCK)
         if str(uid) == self.uid:
             self.mpd_to_card()
 
     def start_playback(self):
         self.connect_mpd()
-        status, uid = self.authenticate(settings.PYMC_BLOCK)
+        status, uid = self.authenticate(PYMC_BLOCK)
         if not str(uid) == self.uid:
             self.read_pymc()
             self.uid = str(uid)
@@ -112,7 +112,7 @@ class PyMC():
     def stop_playback(self):
         self.connect_mpd()
         self.mpd.stop()
-        status, uid = self.authenticate(settings.PYMC_BLOCK)
+        status, uid = self.authenticate(PYMC_BLOCK)
         if str(uid) == self.uid:
             self.mpd_to_card()
 
@@ -129,20 +129,20 @@ class PyMC():
 
     def read_playlists(self):
         try:
-            with open(settings.PLAYLIST_PATH, 'r') as _in:
+            with open(PLAYLIST_PATH, 'r') as _in:
                 self.playlists = json.load(_in)
         except:
             print('could not open playlists file')
 
     def write_playlists(self):
         try:
-            with open(settings.PLAYLIST_PATH, 'w') as _out:
+            with open(PLAYLIST_PATH, 'w') as _out:
                 self.playlists = json.dump(_out, self.playlists)
         except:
             print('could not write playlists file')
 
     def create_playlist(playlistname):
-        status, uid = self.authenticate()
+        status, uid = self.authenticate(PYMC_BLOCK)
         if status == self.MIFAREReader.MF_OK:
             self.connect_mpd()
             self.mpd.save(playlistname)
